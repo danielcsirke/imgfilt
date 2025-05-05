c5ac163ff7d810d9a2d697d7322410590bb73c59
danielcsirke <danielcsirke95@gmail.com>
2025-05-05 16:37:02 +0200

    minor changes

--- include/Exceptions.hpp
+++ include/Exceptions.hpp
@@ -2,18 +2,19 @@
 #include <string>
 
 class FileOpenException : public std::runtime_error {
-
+public
+    FileOpenException(std::string msg);
 };
 
 class InvalidFormatException : public std::runtime_error {
-
+    InvalidFormatException(std::string msg);
 };
 
 class InvalidFilterParameterException : public std::runtime_error {
-
+    InvalidFilterParameterException(std::string msg);
 };
 
 class InvalidFilterTypeException : public std::runtime_error {
-
+    InvalidFilterTypeException(std::string msg);
 };
 
