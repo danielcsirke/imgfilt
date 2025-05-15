dbe8642daa1aada3bbe03f1c680c1e34e68b7363
danielcsirke <danielcsirke95@gmail.com>
2025-05-15 18:53:49 +0200

    idk what happened

--- include/Exceptions.hpp
+++ include/Exceptions.hpp
@@ -2,19 +2,22 @@
 #include <string>
 
 class FileOpenException : public std::runtime_error {
-public
+public:
     FileOpenException(std::string msg);
 };
 
 class InvalidFormatException : public std::runtime_error {
+public:
     InvalidFormatException(std::string msg);
 };
 
 class InvalidFilterParameterException : public std::runtime_error {
+public:
     InvalidFilterParameterException(std::string msg);
 };
 
 class InvalidFilterTypeException : public std::runtime_error {
+public:
     InvalidFilterTypeException(std::string msg);
 };
 
